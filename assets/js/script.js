714041df15eb2d58e16c0ac02f5402c4b1ceeea9
vomkaraxioned <omkarv@axioned.com>
2022-08-26 14:03:17 +0530

    css added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
 /* Author: 
 
 */
+const slides = document.getElementsByClassName("slide");
+const prev = document.querySelector(".prev");
+const nxt = document.querySelector(".next")
+const buttons = document.querySelectorAll(".btn");
+let prevIndex = 0;
+let currentIndex = 0;
+buttons.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        currentIndex = index;
+    });
+});
 
 $(document).ready(function() {
-    $('.your-class').slick();
-});
+    $('.slider').slick({
+        dots: true,
+        arrows: true
+    });
+})
