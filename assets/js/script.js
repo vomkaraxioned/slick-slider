b665bd40cee51ef473ff9c55bdfdb43c4a18965f
vomkaraxioned <omkarv@axioned.com>
2022-08-26 16:17:36 +0530

    js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,11 @@
 /* Author: 
 
 */
-const slides = document.getElementsByClassName("slide");
-const prev = document.querySelector(".prev");
-const nxt = document.querySelector(".next")
-const buttons = document.querySelectorAll(".btn");
-let prevIndex = 0;
-let currentIndex = 0;
-buttons.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-        currentIndex = index;
-    });
-});
+
 
 $(document).ready(function() {
     $('.slider').slick({
-        dots: true,
-        arrows: true
+        arrow: true,
+        dots: true
     });
 })
